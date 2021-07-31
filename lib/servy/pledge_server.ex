2b0d0859242882ee553f8d934f0e504c61427492
jwandekoken <julio.wandekoken@paketa.com.br>
2021-07-31 19:56:44 -0300

    refactoring stateful process toward a GenServer

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,91 +1,115 @@
-defmodule Servy.PledgeServer do
-
-  @process_name :pledge_server
-
-  # Client interface
-  def start do
-    IO.puts("Starting the pledge server...")
-
+defmodule Servy.GenericServer do
+  def start(callback_module, initial_state, name) do
     # spawn will return the pid of the generated process
-    pid = spawn(__MODULE__, :listen_loop, [[]])
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
 
     # https://hexdocs.pm/elixir/Process.html#register/2
-    Process.register(pid, @process_name)
+    Process.register(pid, name)
 
     # we are returning the pid just in case if the caller needs it
     pid
   end
 
-  def create_pledge(name, amount) do
-    send(@process_name, {self(), :create_pledge, name, amount})
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
 
     receive do
-      {:response, created_pledge_id} -> created_pledge_id
+      {:response, response} -> response
     end
+  end
+
+  def cast(pid, message) do
+    send(pid, {:cast, message})
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send(sender, {:response, response})
+        listen_loop(new_state, callback_module)
+
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+
+      unexpected ->
+        IO.puts("Unexpected message: #{inspect(unexpected)}")
+        listen_loop(state, callback_module)
+    end
+  end
+end
+
+defmodule Servy.PledgeServer do
+  alias Servy.GenericServer
+
+  @process_name :pledge_server
+
+  def start() do
+    GenericServer.start(__MODULE__, [], @process_name)
+  end
+
+  def create_pledge(name, amount) do
+    GenericServer.call(@process_name, {:create_pledge, name, amount})
   end
 
   def recent_pledges() do
-    send(@process_name, {self(), :recent_pledges})
-
-    receive do
-      {:response, pledges} -> pledges
-    end
+    GenericServer.call(@process_name, :recent_pledges)
   end
 
   def total_pledged() do
-    send(@process_name, {self(), :total_pledged})
-
-    receive do
-      {:response, total} -> total
-    end
+    GenericServer.call(@process_name, :total_pledged)
   end
 
-  # Server
-  def listen_loop(state) do
-    receive do
-      # sender is a pid
-      {sender, :create_pledge, name, amount} ->
-        {:ok, id} = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(state, 2)
-        new_state = [{name, amount} | most_recent_pledges]
-        send(sender, {:response, id})
-        listen_loop(new_state)
-
-      {sender, :recent_pledges} ->
-        send(sender, {:response, state})
-        listen_loop(state)
-
-      {sender, :total_pledged} ->
-        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-        send(sender, {:response, total})
-        listen_loop(state)
-
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect(unexpected)}"
-        listen_loop(state)
-    end
+  def clear do
+    GenericServer.cast(@process_name, :clear)
   end
 
   defp send_pledge_to_service(_name, _amount) do
     # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
+
+  # Server Callbacks
+  def handle_call(:total_pledged, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
+    {total, state}
+  end
+
+  def handle_call(:recent_pledges, state) do
+    {state, state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state, 2)
+    new_state = [{name, amount} | most_recent_pledges]
+    {id, new_state}
+  end
+
+  def handle_cast(:clear, _state) do
+    []
+  end
 end
 
-# alias Servy.PledgeServer
+# Testing code
+alias Servy.PledgeServer
 
-# pid = PledgeServer.start()
+pid = PledgeServer.start()
 
-# send(pid, {:stop, "hammertime"})
+send(pid, {:stop, "hammertime"})
 
-# PledgeServer.create_pledge("larry", 10) |> IO.inspect
-# PledgeServer.create_pledge("moe", 20) |> IO.inspect
-# PledgeServer.create_pledge("curly", 30) |> IO.inspect
-# PledgeServer.create_pledge("daisy", 40) |> IO.inspect
-# PledgeServer.create_pledge("grace", 50) |> IO.inspect
+PledgeServer.create_pledge("larry", 10) |> IO.inspect()
+PledgeServer.create_pledge("moe", 20) |> IO.inspect()
+PledgeServer.create_pledge("curly", 30) |> IO.inspect()
+PledgeServer.create_pledge("daisy", 40) |> IO.inspect()
 
-# PledgeServer.recent_pledges() |> IO.inspect
+PledgeServer.clear()
 
-# PledgeServer.total_pledged() |> IO.inspect
+PledgeServer.create_pledge("grace", 50) |> IO.inspect()
 
-# Process.info(pid, :messages) |> IO.inspect
+PledgeServer.recent_pledges() |> IO.inspect()
+
+PledgeServer.total_pledged() |> IO.inspect()
+
+Process.info(pid, :messages) |> IO.inspect()
