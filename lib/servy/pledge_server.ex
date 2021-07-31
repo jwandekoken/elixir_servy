f054881fbe7f92baf2cef6bab53ede4d35d29edb
jwandekoken <julio.wandekoken@paketa.com.br>
2021-07-31 10:57:23 -0300

    commented test code

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -72,20 +72,20 @@ defmodule Servy.PledgeServer do
   end
 end
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-pid = PledgeServer.start()
+# pid = PledgeServer.start()
 
-send(pid, {:stop, "hammertime"})
+# send(pid, {:stop, "hammertime"})
 
-PledgeServer.create_pledge("larry", 10) |> IO.inspect
-PledgeServer.create_pledge("moe", 20) |> IO.inspect
-PledgeServer.create_pledge("curly", 30) |> IO.inspect
-PledgeServer.create_pledge("daisy", 40) |> IO.inspect
-PledgeServer.create_pledge("grace", 50) |> IO.inspect
+# PledgeServer.create_pledge("larry", 10) |> IO.inspect
+# PledgeServer.create_pledge("moe", 20) |> IO.inspect
+# PledgeServer.create_pledge("curly", 30) |> IO.inspect
+# PledgeServer.create_pledge("daisy", 40) |> IO.inspect
+# PledgeServer.create_pledge("grace", 50) |> IO.inspect
 
-PledgeServer.recent_pledges() |> IO.inspect
+# PledgeServer.recent_pledges() |> IO.inspect
 
-PledgeServer.total_pledged() |> IO.inspect
+# PledgeServer.total_pledged() |> IO.inspect
 
-Process.info(pid, :messages) |> IO.inspect
+# Process.info(pid, :messages) |> IO.inspect
